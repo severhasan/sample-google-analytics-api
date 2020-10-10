d537f930a14d6a28b96210f96dcfe42dc5146b01
Hasan Sever <pal_sever@live.com>
2020-10-10 19:48:42 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const { getData } = require('./lib/ga');
+
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+
+app.get('/analytics', async (req, res) => {
+    const data = await getData();
+    res.send(data);
+    // res.sendFile('view/analytics.html', { root: __dirname });
+})
+
+app.listen(3000, () => console.log('> Listening on port 3000'));
